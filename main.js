f3d3453300095ba7609b4805721cc3c57704c2d9
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-24 04:16:17 +0900

    Conbination of v-for and v-if.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var app = new Vue({
     el: "#app",
     data: {
-        ok: true,
-        type:""
+        list:[
+            {id:1, name:'スライム', hp:100},
+            {id:2, name:'ゴブリン', hp:200},
+            {id:3, name:'ドラゴン', hp:500},
+        ]
     },
 })
