4b3cbb654c5716310a5e111f78335e9e1a2be7e5
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-24 04:56:34 +0900

    Remove a data list element.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var app = new Vue({
     el: "#app",
     data: {
-        name:"キマイラ",
         list:[
             {id:1, name:'スライム', hp:100},
             {id:2, name:'ゴブリン', hp:200},
@@ -9,16 +8,8 @@ var app = new Vue({
         ]
     },
     methods: {
-        doAdd: function () {
-            var max = this.list.reduce(function (a, b) {
-                return a > b.id ? a : b.id
-            }, 0)
-
-            this.list.push({
-                id: max + 1,
-                name: this.name,
-                hp: 500
-            })
+        doRemove: function (index) {
+            this.list.splice(index, 1)
         }
     }
 })
