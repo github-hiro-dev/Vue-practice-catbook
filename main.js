fcd8d5449d050f0114966e5d9fb70fc06a04a7aa
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-25 03:42:23 +0900

    Use $el and $ref to access DOM.

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 var app = new Vue({
     el: "#app",
-    data: {
-        res:{}
-    },
-    created: function () {
-        axios.get("http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=my-api-key")
-            .then(function (response) {
-            this.res = response.data
-            }.bind(this))
-            .catch(function (e) {
-            console.error(e)
-        })
+    mounted: function () {
+        console.log(this.$el)
+        console.log(this.$refs.hello)
     }
 })
