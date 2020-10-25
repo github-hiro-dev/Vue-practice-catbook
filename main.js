8695a0e41ed078b9259445daf4337ca75da9bbf5
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-26 02:26:23 +0900

    Use computed property.

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 var app = new Vue({
     el: "#app",
     data:{
-        scrollY: 0,
-        timer: null
+        width: 800
     },
-    created: function () {
-        window.addEventListener('scroll', this.handleScroll)
-    },
-    beforeDestroy: function () {
-        window.removeEventListener('scroll', this.handleScroll)
-    },
-    methods: {
-        handleScroll: function () {
-            if (this.timer === null) {
-                this.timer = setTimeout(function () {
-                    this.scrollY = window.scrollY
-                    clearTimeout(this.timer)
-                    this.timer = null
-                }.bind(this), 200)
-            }
+    computed: {
+        halfWidth: function () {
+            return this.width / 2
         }
     }
 })
