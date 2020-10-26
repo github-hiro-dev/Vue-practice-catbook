a887f408beff4dacc8f339da4dcbdc6a996d841d
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-26 20:38:09 +0900

    Use filter.

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 var app = new Vue({
     el: "#app",
     data:{
-        list: [],
-        current: '',
-        topics: [
-            {value:'vue', name:'Vue.js'},
-            {value:'jQuery', name:'jQuery'}
-        ],
-        order: false
+        price:19800
     },
-    watch: {
-        current: function (val) {
-            console.log(val)
-            axios.get('https://api.github.com/search/repositories', {
-                params:{q:'topic:' + val}
-            }).then(function (response) {
-                this.list = response.data.items
-            }.bind(this))
+    filters: {
+        localeNum: function (val) {
+            return val.toLocaleString()
         }
     }
 })
