0e92f820a6033d1500787d671dd11278efaa139c
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-29 15:47:51 +0900

    Defferent template definition.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var myComponent = {
-    template: '<section class="my-comp"><header><slot name="header">デフォルトタイトル</slot></header><div class="content"><slot></slot></div><footer><slot name="footer"></slot></footer></section>',
+    template: '#my-component',
 }
 
 var app = new Vue({
