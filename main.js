71f952fc9b36e07862d351c11cfb629ad76fb9cb
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-29 16:11:17 +0900

    Switch template with :is property.

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-var myComponent = {
-    template: '#my-component',
+var myComponentA = {
+    template: '<div class="my-component-a">component A</div>',
+}
+
+var myComponentB = {
+    template: '<div class="my-component-b">component B</div>',
 }
 
 var app = new Vue({
     el: "#app",
+    data: {
+        componentTypes: ['my-component-a', 'my-component-b'],current:0
+    },
+    computed: {
+        component: function () {
+            return this.componentTypes[this.current]
+        }
+    },
     components: {
-        'my-component': myComponent
+        'my-component-a': myComponentA,
+        'my-component-b': myComponentB,
     }
 })
