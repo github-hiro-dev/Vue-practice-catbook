b4eeb441070528c09b2bf4b8d285f78e21b969f4
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-30 01:38:12 +0900

    Vue transition.

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-var compBoard = {
-    template: '<div>Message board</div>',
-}
-
-var compForm = {
-    template: '<div>Form<textarea v-model="message"></textarea></div>',
-    data: function () {
-        return { message: ''}
-    },
-    activated: function () {
-        console.log('activated!')
-    },
-    deactivated: function () {
-        console.log('deactivated!')
-    },
-}
-
 var app = new Vue({
     el: "#app",
     data: {
-        current:'comp-board'
+        show: true
     },
-    components: {
-        'comp-board': compBoard,
-        'comp-form': compForm,
-    }
 })
