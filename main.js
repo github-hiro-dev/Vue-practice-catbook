d44305ae2893f12c6a8a5f7ec6aa30c22a83c6cf
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-30 09:53:05 +0900

    SVG transition.

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+var myCircle = {
+    template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
+    props: {
+        fill: String
+    }
+}
+
 var app = new Vue({
     el: "#app",
     data: {
-        order: false,
-        list: [
-            { id: 1, name: 'りんご', price: 100},
-            { id: 2, name: 'ばなな', price: 200},
-            { id: 3, name: 'いちご', price: 300},
-        ]
+        toggle: false
     },
     computed: {
-        sortedList: function () {
-            return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+        fill: function () {
+            return this.toggle ? 'lightpink' : 'skyblue'
         }
+    },
+    components: {
+        'my-circle': myCircle
     }
 })
