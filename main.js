350e03466b29df213ea4b99f1f3ae67067e1c111
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-30 09:36:56 +0900

    Transition group tag.

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var app = new Vue({
     el: "#app",
     data: {
-        show: true
+        order: false,
+        list: [
+            { id: 1, name: 'りんご', price: 100},
+            { id: 2, name: 'ばなな', price: 200},
+            { id: 3, name: 'いちご', price: 300},
+        ]
     },
+    computed: {
+        sortedList: function () {
+            return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+        }
+    }
 })
