68fac7e166b81376839ccd2ccaf45fc406d8ca3e
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-11-02 20:23:19 +0900

    Add simple SPA.

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/product">商品情報</router-link>
+      </nav>
     </div>
     <router-view/>
   </div>
