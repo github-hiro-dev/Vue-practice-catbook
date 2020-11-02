4c6b3cabc2e2c09ecb6007456483b399e7ea2903
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-11-02 19:57:04 +0900

    Install vue router.

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    <p>{{message}}</p>
-    <EditForm />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import EditForm from './components/EditForm.vue'
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
-  components: {
-    EditForm
-  },
-  computed:{
-    message(){
-      return this.$store.getters.message
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
-</script>
+</style>
