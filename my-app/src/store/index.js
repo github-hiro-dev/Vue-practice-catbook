c3f638af86016d5e0f1de72b1aaba0318590e56e
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-11-02 16:02:57 +0900

    Share vuex store between components.

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -5,14 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    message: '初期メッセージ'
+  },
+  getters: {
+    message(state){return state.message}
   },
   mutations: {
-    increment(state) {
-      state.count++
+    setMessage(state, payload) {
+      state.message = payload.message
     }
   },
   actions: {
+    doUpdate({ commit }, message) {
+      commit('setMessage', {message})
+    }
   },
   modules: {
   }
